d38a7dfee787f672cff4c365583aa13fca8b1f0c
Jack Yuan <jackyuan110@gmail.com>
2025-06-19 18:07:37 -0700

    fix

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -8,15 +8,16 @@ import 'react-notion-x/src/styles.css'
 const notion = new NotionAPI()
 const recordMap = await notion.getPage('067dd719a912471ea9a3ac10710e7fdf')
 
-interface PageProps {
-  params: {
-    id: string
-  }
-}
+// interface PageProps {
+//   params: {
+//     id: string
+//   }
+// }
 
-export default function ProjectPage({ params }: PageProps) {
+// export default function ProjectPage({ params }: PageProps) {
+export default function ProjectPage() {
 
-  console.log(params)
+  // console.log(params)
 
   return (
     <div style={{ maxWidth: '800px', margin: '0 auto', padding: '2rem' }}>
